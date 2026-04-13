f26ffc8f0ceb98e22228f474bfa2cd265453c22e
Zhengyang <z5545363@ad.unsw.edu.au>
2026-04-13 11:53:10 +1000

    1

---
+++ 1.jsx
@@ -0,0 +1,13 @@
+function MyButton() {
+    return (
+        <button>i am a button</button>
+    );
+}
+export default function MyApp() {
+    return (
+        <div className="Welcome-msg">
+        <h1>welcome to MyApp</h1>
+        <MyButton/>
+        </div>
+    );
+}
--- self/JS/variables.js
+++ self/JS/variables.js
@@ -1,14 +1,5 @@
 
-function area_of_circle(r, pi) {
-    if (arguments.length == 1) {
-        return r*r*3.14;
-    }
-    else {
-        return pi*r*r;
-    }
-}
-if (area_of_circle(2) === 12.56 && area_of_circle(2, 3.1416) === 12.5664) {
-    console.log('测试通过');
-} else {
-    console.log('测试失败');
-}
+let gfs = ['1', '2', '3'];
+let [first, second] = gfs;
+console.log(first);
+console.log(second);
