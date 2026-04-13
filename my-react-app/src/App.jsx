20eae3fe820192c89f50dc91edc72eaaaaf6cc6f
Zhengyang Lu <z5545363@ad.unsw.edu.au>
2026-04-13 18:03:07 +1000

    12

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return <>  <form className='new-item-form'>
+    <div className='form-row'>
+      <label htmlFor='item'>New Item</label>
+      <input type = 'text' id ='item'></input>
+    </div>
+    <button className='addbtn'>Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul>
+    <label>
+      <input type = 'checkbox'>
+      Item 1
+      </input>
+    </label>
+  </ul>
+  </>
+
+
+}
