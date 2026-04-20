873bf776c5b2148ad3e7d3bd2d15d4410d5aad04
Zhengyang <z5545363@ad.unsw.edu.au>
2026-04-20 13:26:31 +1000

    123

--- ti/my23t1/src/components/Dashboard.jsx
+++ ti/my23t1/src/components/Dashboard.jsx
@@ -1,2 +1,13 @@
 import React from 'react';
-import "./"
+import 
+const Dashboard = () => {
+    const [wons, setWons] = useState(0);
+
+    return (
+        <>
+        <div className = "text">Please choose an option from the navbar.</div>
+        <span>Games won: {wons}</span>
+        <button className = "reset-btn"></button>
+        </>
+    )
+}
